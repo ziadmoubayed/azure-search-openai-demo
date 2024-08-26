da871e1d661dd15d3685e38525e2745fdf33f6e2
Malek el Khazen <khazen@gmail.com>
2024-08-25 19:41:08 -0500

    Update Chat.tsx

--- app/frontend/src/pages/chat/Chat.tsx
+++ app/frontend/src/pages/chat/Chat.tsx
@@ -426,7 +426,7 @@ const Chat = () => {
                     <div className={styles.chatInput}>
                         <QuestionInput
                             clearOnSend
-                            placeholder="Type a new question (e.g. does my plan cover annual eye exams?)"
+                            placeholder="Type a new question (e.g.Type a new question (e.g. what is the difference between Quantexa and Sayari Labs?)"
                             disabled={isLoading}
                             onSend={question => makeApiRequest(question)}
                             showSpeechInput={showSpeechInput}
